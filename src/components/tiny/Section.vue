c36b1e68f4d54cda1d4b220d0783cc523e1b1ee4
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2021-07-11 00:45:07 -0300

    Vue setup

---
+++ src/components/tiny/Section.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <header>
+      <h2>{{ title }}</h2>
+      <h3>{{ subtitle }}</h3>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/tiny/SocialLinks.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul class="socialLinks">
+    <li>
+      <a target="_blank" href="https://www.instagram.com/caiu.correa/">
+        <i class="fab fa-instagram"></i>
+        caiu.correa
+        <i class="fas fa-external-link-alt"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://www.linkedin.com/in/caio-corrêa-chaves/">
+        <i class="fab fa-linkedin"></i>
+        Caio Corrêa Chaves
+        <i class="fas fa-external-link-alt"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://github.com/Gaius10">
+        <i class="fab fa-github"></i>
+        Gaius10
+        <i class="fas fa-external-link-alt"></i>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+</script>
+
+<style>
+ul.socialLinks {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+}
+
+ul.socialLinks li {
+  display: block;
+}
+
+ul.socialLinks li a {
+  /* background: #252525; */
+  background-color: var(--colorDocumentSecondary);
+  color: black;
+  display: block;
+  padding: 1rem 0;
+  border-radius: 1rem;
+}
+
+@media only screen and (min-width: 1200px) {
+  ul.socialLinks li a {
+    transition-duration: 0.4s;
+    cursor: pointer;
+  }
+  ul.socialLinks li a:hover {
+    background-color: #bbb;
+  }
+}
+</style>
